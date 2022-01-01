b49e811437b3d2bf2ab45de2fee51acb86ae7a71
Chewbakkah <chewbakkah@gmail.com>
2022-01-01 12:43:34 -0600

    html file created

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [user, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(user, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
